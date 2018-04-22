1d254a55eb3b7f1566eef178905530e5d73fa820
8989670898 <raiparth2@gmail.com>
2018-04-22 02:48:37 -0700

    comitting my first project in scm

---
+++ operatoroverloading.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+class Point {
+  int x, y;
+public:
+  Point() {} //prajjwal will add here
+
+  void show() {
+    cout << x << " ";
+    cout << y << "\n";
+  }
+
+  friend Point operator+(Point op1, Point op2); // now a friend
+  Point operator-(Point op2);
+  Point operator=(Point op2);
+  Point operator++();
+};
+
+// Nishkarsh will addd here
+Point operator+(Point op1, Point op2)
+{
+
+}
+ 
+// Overload - for Point.
+Point Point::operator-(Point op2)
+{
+  Point temp;
+ 
+  // notice order of operands
+  temp.x = x - op2.x;
+  temp.y = y - op2.y;
+ 
+  return temp;
+}
+ 
+// Prakhar will work here
+Point Point::operator=(Point op2)
+{
+
+}
+ 
+// Overload ++ for Point.
+Point Point::operator++()
+{
+  x++;
+  y++;
+ 
+  return *this;
+}
+ 
+int main()
+{
+  Point ob1(10, 20), ob2( 5, 30);
+ 
+  ob1 = ob1 + ob2;
+  ob1.show();
+ 
+  return 0;
+}
